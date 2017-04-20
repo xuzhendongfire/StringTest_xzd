ded285776afc1dbade1993c2df0a78c8b7c5c20e
xzd8812555 <839638952@qq.com>
2017-04-20 22:12:56 +0800

    第一次
    测试

---
+++ src/test/test.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class test {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = "";
+		str = s.nextLine();
+		System.out.println(maxString(str));
+	}
+
+	@Test
+	public void testNoChar() {
+		String str = "";
+		System.out.println(maxString(str));
+	}
+
+	public static int maxString(String str) {
+		int maxLength = 0;
+		if (str.length() == 0)
+			return 0;
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (map.get(ch) != null)
+				return maxLength;
+			else {
+				map.put(ch, 1);
+				maxLength++;
+			}
+		}
+		return maxLength;
+	}
+}
